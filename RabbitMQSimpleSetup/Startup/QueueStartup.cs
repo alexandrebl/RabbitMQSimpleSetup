dc2600d50bedb20ed882d204444f2162f9c056c2
Tiago Santos <tiago.santos@stone.com.br>
2018-10-17 17:34:55 -0300

    implemented option to queue additional structure

--- RabbitMQSimpleSetup/Startup/QueueStartup.cs
+++ RabbitMQSimpleSetup/Startup/QueueStartup.cs
@@ -2,29 +2,35 @@
 using RabbitMQSimpleSetup.Domain;
 using System.Collections.Generic;
 
-namespace RabbitMQSimpleSetup.Startup {
-    public sealed class QueueStartup {
-        public void QueueInit(IModel channel, string exchange, IEnumerable<QueueBindingInfo> queueBindings, bool enableDeadLettering = true) {
+namespace RabbitMQSimpleSetup.Startup
+{
+    public sealed class QueueStartup
+    {
+        public void QueueInit(IModel channel, string exchange, IEnumerable<QueueBindingInfo> queueBindings, IEnumerable<QueueAddicionalStructure> queueAddicionalStructures = null,
+            bool enableDeadLettering = true)
+        {
             var exchangeSetup = new ExchangeSetup();
             var queueSetup = new QueueSetup();
 
             exchangeSetup.ExchangeDeclare(channel, exchange);
 
-            foreach (var queueBinding in queueBindings) {
+            foreach (var queueBinding in queueBindings)
+            {
                 var errorQueueName = $"{queueBinding.Queue}.error";
-                var errorRouteName = exchange + "." + errorQueueName;
+                var errorRouteName = $"{exchange}.{errorQueueName}";
                 var processQueueName = $"{queueBinding.Queue}.processing";
                 var processRouteName = queueBinding.Route;
-                var logQueueName = $"{queueBinding.Queue}.log";
 
-                queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData {
+                queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData
+                {
                     QueueName = errorQueueName,
                     RouteName = errorRouteName,
                     ExchangeName = exchange,
                     MaxPriority = queueBinding.Queue.MaxPriority
                 }, autoDelete: false, exclusive: true, durable: false);
 
-                queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData {
+                queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData
+                {
                     QueueName = processQueueName,
                     RouteName = processRouteName,
                     ExchangeName = exchange,
@@ -32,13 +38,18 @@ namespace RabbitMQSimpleSetup.Startup {
                     DeadLetterRouteName = (enableDeadLettering) ? errorRouteName : null
                 }, autoDelete: false, exclusive: true, durable: false);
 
-                queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData {
-                    QueueName = logQueueName,
-                    RouteName = processRouteName,
-                    ExchangeName = exchange,
-                    Lazy = true,
-                    MaxPriority = queueBinding.Queue.MaxPriority,
-                }, autoDelete: false, exclusive: true, durable: false);
+                if (queueAddicionalStructures is null) continue;
+                foreach (var queueAddicionalStructure in queueAddicionalStructures)
+                {
+                    queueSetup.QueueDeclareAndBind(channel, new QueueConfigurationData
+                    {
+                        QueueName = $"{queueBinding.Queue}.{queueAddicionalStructure.SuffixQueueName}",
+                        RouteName = processRouteName,
+                        ExchangeName = exchange,
+                        Lazy = queueAddicionalStructure.IsLazy,
+                        MaxPriority = queueBinding.Queue.MaxPriority,
+                    }, queueAddicionalStructure.AutoDelete, queueAddicionalStructure.Exclusive, queueAddicionalStructure.Durable);
+                }
             }
         }
     }
